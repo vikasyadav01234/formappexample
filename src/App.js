d5fe67f69dcbcd8d9e0fe3300876e9566cbb9071
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-03-29 09:12:27 +0530

    main

--- src/App.js
+++ src/App.js
@@ -9,11 +9,12 @@ function App() {
   function changeFirstHandler(event){
     //console.log("Printing Last Name")
     //console.log(event.target.value);
-    setFirstName(event.target.value)
+    setFirstName(event.target.value);
   }
   function changeLastHandler(event){
     //console.log("Printing Last Name")
     //console.log(event.target.value);
+    
   }
   return (
    <div className='App'>
